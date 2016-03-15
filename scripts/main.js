d51236530281a0edfcde6e939392d6a5377ec31c
gregoirepuget <gregoirepuget@gmail.com>
2016-03-15 14:11:04 +0100

    Champs nombre

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
 
+  $("#numberField").attr("max",citations.length-1);
+  
+  $('#cookie_form').on("submit",function(e){
+    e.preventDefault();
+    var nombre=$("#numberField").val();
+    
+    $("#fortune_cookie").empty();
+    $("#fortune_cookie").append("<p>"+citations[nombre]["citation"]+"</p>");
+    $("#fortune_cookie").append("<span>"+citations[nombre]["auteur"]+"</span>");
+  });
+  
+  $("#numberField").on("blur",function(){
+    var nombre=$(this).val();
+    
+    $("#fortune_cookie").empty();
+    $("#fortune_cookie").append("<p>"+citations[nombre]["citation"]+"</p>");
+    $("#fortune_cookie").append("<span>"+citations[nombre]["auteur"]+"</span>");
+    
+  });
+  
+  
   $("#fortune_button").on('click', function(event){
   
     event.preventDefault();
