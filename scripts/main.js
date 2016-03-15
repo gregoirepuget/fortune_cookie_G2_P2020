3e8f5d2469028945b2ebcb3a1ac6de73514f902e
gregoirepuget <gregoirepuget@gmail.com>
2016-03-15 15:31:57 +0100

    menu

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,20 @@ $(document).ready(function(){
 
   $("#numberField").attr("max",citations.length-1);
   
+  var listOptions='';
+  for(var i=0; i < citations.length; i++)
+  {
+    listOptions += "<option value='"+i+"'>"+citations[i]["auteur"]+"</option>";
+  }
+  $("#authorField").html(listOptions);
+  //console.log(listOptions);
+  
+  $("#authorField").on("change",function(){
+     var nombre=$(this).val();
+      addCitation(nombre);
+  });
+  
+  
   $('#cookie_form').on("submit",function(e){
     e.preventDefault();
     var nombre=$("#numberField").val();
@@ -36,6 +50,39 @@ $(document).ready(function(){
     $("#fortune_cookie").append("<p>"+citations[nombre]["citation"]+"</p>");
     $("#fortune_cookie").append("<span>"+citations[nombre]["auteur"]+"</span>");
   }
+  
+  
+  $("#nom p").on("blur", function(){
+    localStorage.setItem("nom",$(this).html());
+  });
+  
+  var nom= localStorage.getItem("nom");
+  console.log(nom);
+  if(nom != null)
+  {
+    $("#nom p").html(nom);
+  }
+  
+  $("#nom span").on("click",function(){
+    localStorage.removeItem("nom");
+    $("#nom p").html("Votre nom");
+  });
+  
+  $("#menu button").on('click', function(){
+  
+    $(this).toggleClass('is-active');
+    $("#menuContent").toggleClass("open");
+  });
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
   
   
